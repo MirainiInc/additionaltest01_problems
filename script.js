841c7303d6042b5832f77c2555b1cb7a81fa4030
Junji Maruyama <153514560+j-maruyama@users.noreply.github.com>
2024-01-14 16:04:51 +0900

    chore: 正解リポジトリに加えた差分をマージした

--- script.js
+++ script.js
@@ -8,148 +8,142 @@ const quizBox = document.querySelector('.quiz-box');
 const resultBox = document.querySelector('.result-box');
 const tryAgainBtn = document.querySelector('.tryAgain-btn');
 const goHomeBtn = document.querySelector('.goHome-btn');
-
-startBtn.onclick = () => {
-    popupInfo.classList.add('active');
-    main.classList.add('active');
-}
-
-exitBtn.onclick = () => {
-}
-
-continueBtn.onclick = () => {
-    quizSection.classList.add('active');
-    popupInfo.classList.remove('active');
-    main.classList.remove('active');
-    quizBox.classList.add('active');
-
-    showQuestions(0);
-    questionCounter(1);
-    headerScore();
-}
-
-tryAgainBtn.onclick = () => {
-    quizBox.classList.add('active');
-    nextBtn.classList.remove('active');
-    resultBox.classList.remove('active');
-
-    headerScore();
-}
-
-goHomeBtn.onclick = () => {
-    quizSection.classList.remove('active');
-    nextBtn.classList.remove('active');
-    resultBox.classList.remove('active');
-
-    questionCount = 0;
-    questionNumb = 1;
-    userScore = 0;
-    showQuestions(questionCount);
-    questionCounter(questionNumb);
-}
-
-let questionCount = 0;
-let questionNumb = 1;
-let userScore = 0;
-
 const nextBtn = document.querySelector('.next-btn');
 
-nextBtn.onclick = () => {
-    if (questionCount < questions.length) {
-        questionCount++;
-        showQuestions(questionCount);
+let questionCount = 0;
+let userScore = 0;
 
-        questionNumb++;
-        questionCounter(questionNumb);
+startBtn.addEventListener('click', () => {
+  popupInfo.classList.add('active');
+  main.classList.add('active');
+})
 
-        nextBtn.classList.remove('active');
-    }
-    else {
-        showResultBox();
-    }
-}
+exitBtn.addEventListener('click', () => {
+})
+
+continueBtn.addEventListener('click', () => {
+  quizSection.classList.add('active');
+  popupInfo.classList.remove('active');
+  main.classList.remove('active');
+  quizBox.classList.add('active');
+
+  showQuestions(0);
+  headerScore();
+})
+
+tryAgainBtn.addEventListener('click', () => {
+  quizBox.classList.add('active');
+  nextBtn.classList.remove('active');
+  resultBox.classList.remove('active');
+
+  headerScore();
+})
+
+goHomeBtn.addEventListener('click', () => {
+  quizSection.classList.remove('active');
+  nextBtn.classList.remove('active');
+  resultBox.classList.remove('active');
+
+  questionCount = 0;
+  userScore = 0;
+  showQuestions(questionCount);
+})
+
+nextBtn.addEventListener('click', () => {
+  if (questionCount < questions.length) {
+    questionCount++;
+    showQuestions(questionCount);
+
+    nextBtn.classList.remove('active');
+  } else {
+    showResultBox();
+  }
+})
 
 const optionList = document.querySelector('.option-list');
 
 // getting questions and options from array
 function showQuestions(index) {
-    const questionText = document.querySelector('.question-text');
-    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+  questionCounter(index)
 
-    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+  const questionText = document.querySelector('.question-text');
+  questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+  const optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
         <div class="option"><span>${questions[index].options[1]}</span></div>
         <div class="option"><span>${questions[index].options[2]}</span></div>
         <div class="option"><span>${questions[index].options[3]}</span></div>`;
 
-    optionList.innerHTML = optionTag;
+  optionList.innerHTML = optionTag;
 
-    const option = document.querySelectorAll('.option');
-    for (let i = 0; i < option.length; i++) {
-        option[i].setAttribute('onclick', 'optionSelected(this)');
-    }
+  const options = document.querySelectorAll('.option');
+  for (const option of options) {
+    option.addEventListener('click', function () {
+      optionSelected(this);
+    })
+  }
 }
 
 function optionSelected(answer) {
-    let userAnswer = answer.textContent;
-    let correctAnswer = questions[questionCount].answer;
-    let allOptions = optionList.children.length;
+  const userAnswer = answer.textContent;
+  const correctAnswer = questions[questionCount].answer;
+  const allOptions = optionList.children;
 
-    userScore += 1;
+  userScore += 1;
 
-    if (userAnswer == correctAnswer) {
-        answer.classList.add('correct');
-        headerScore();
+  if (userAnswer === correctAnswer) {
+    answer.classList.add('correct');
+    headerScore();
+  } else {
+    answer.classList.add('incorrect');
+
+    // if answer incorrect, auto selected correct answer
+    for (const option of allOptions) {
+      if (option.textContent === correctAnswer) {
+        option.classList.add('correct');
+      }
     }
-    else {
-        answer.classList.add('incorrect');
+  }
 
-        // if answer incorrect, auto selected correct answer
-        for (let i = 0; i < allOptions; i++) {
-            if (optionList.children[i].textContent == correctAnswer) {
-                optionList.children[i].setAttribute('class', 'option correct');
-            }
-        }
-    }
+  // if user has selected, disabled all options
+  for (const option of allOptions) {
+    option.classList.add('disabled');
+  }
 
-    // if user has selected, disabled all options
-    for (let i = 0; i < allOptions; i++) {
-        optionList.children[i].classList.add('disabled');
-    }
-
-    nextBtn.classList.add('active');
+  nextBtn.classList.add('active');
 }
 
 function questionCounter(index) {
-    const questionTotal = document.querySelector('.question-total');
-    questionTotal.textContent = `第${index}問`;
+  const questionTotal = document.querySelector('.question-total');
+  questionTotal.textContent = `第${index + 1}問`;
 }
 
 function headerScore() {
-    const headerScoreText = document.querySelector('.header-score');
-    headerScoreText.textContent = `点数: ${userScore} / ${questions.length}`;
+  const headerScoreText = document.querySelector('.header-score');
+  headerScoreText.textContent = `点数: ${userScore} / ${questions.length}`;
 }
 
 function showResultBox() {
-    quizBox.classList.remove('active');
-    resultBox.classList.add('active');
+  quizBox.classList.remove('active');
+  resultBox.classList.add('active');
 
-    const scoreText = document.querySelector('.score-text');
-    scoreText.textContent = `${questions.length} 問中 ${userScore} 問正解！`;
+  const scoreText = document.querySelector('.score-text');
+  scoreText.textContent = `${questions.length} 問中 ${userScore} 問正解！`;
 
-    const circularProgress = document.querySelector('.circular-progress');
-    const progressValue = document.querySelector('.progress-value');
-    let progressStartValue = -1;
-    let progressEndValue = (userScore / questions.length) * 100;
-    let speed = 20;
+  const circularProgress = document.querySelector('.circular-progress');
+  const progressValue = document.querySelector('.progress-value');
+  let progressStartValue = -1;
+  const progressEndValue = (userScore / questions.length) * 100;
+  const speed = 20;
 
-    let progress = setInterval(() => {
-        progressStartValue++;
+  const progress = setInterval(() => {
+    progressStartValue++;
 
-        progressValue.textContent = `${progressStartValue}%`;
-        circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
+    progressValue.textContent = `${progressStartValue}%`;
+    circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
 
-        if (progressStartValue == progressEndValue) {
-            clearInterval(progress);
-        }
-    }, speed);
+    if (progressStartValue === progressEndValue) {
+      clearInterval(progress);
+    }
+  }, speed);
 }
